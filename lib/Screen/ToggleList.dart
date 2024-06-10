525aad81a72d79cabb7b2132431fef911eed0187
sajid <sajidshaikh12543@gmail.com>
2024-06-10 15:18:44 +0530

    04-06-2024

--- lib/Screen/ToggleList.dart
+++ lib/Screen/ToggleList.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../utils/QuoteList.dart';
 import 'dart:math';
 import 'QuoteModel.dart';
+
 QuoteModel? quoteModel;
 
 class ToggleView extends StatefulWidget {
